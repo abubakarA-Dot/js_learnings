7a8e663437a1ae5f1f0732bae0b010d197dd474d
abubakarA-Dot <abubakarbryar@gmail.com>
2023-05-16 18:37:20 +0500

    Add Filter using keyup event listener

--- html/form_item_list.js
+++ html/form_item_list.js
@@ -1,7 +1,12 @@
 let form_id = document.querySelector('#addForm')
 let itemList = document.querySelector('#items')
+let filterItem = document.querySelector('#filter')
 
 form_id.addEventListener('submit', addFormItem);
+
+itemList.addEventListener('click', removeItem);
+
+filterItem.addEventListener('keyup', filterListItems);
 
 
 function addFormItem(event){
@@ -19,6 +24,32 @@ function addFormItem(event){
     deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
     deleteBtn.appendChild(document.createTextNode('X'));
     li.appendChild(deleteBtn);
-    
+
     itemList.appendChild(li);
+}
+
+function removeItem(event){
+    if (event.target.classList.contains('delete')){
+        if (confirm('Are you sure?')){
+            let li = event.target.parentElement;
+            itemList.removeChild(li);
+        }
+    }
+}
+
+
+function filterListItems(event){
+    let searchText = event.target.value.toLowerCase();
+
+    let items = itemList.querySelectorAll('li');
+    // converting in array first
+    Array.from(items).forEach(function(item){
+        let itemName = item.firstChild.textContent;
+        if (itemName.toLowerCase().indexOf(searchText) != -1){
+            item.style.display = 'block';
+        } else {
+            item.style.display = 'none';
+        }
+
+    })
 }
